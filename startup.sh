ec74ee54fb58dad79a859f5ba4b3c4ce0b7a8eea
mstephens-xsl <matt.stephens@airelogic.com>
2018-09-27 13:56:13 +0100

    Updated environment variable for FHIR server.

--- startup.sh
+++ startup.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 docker pull snoopy2k18/a2si-facade
-docker run -p 8189:8188/tcp -e fhir.restserver.serverBase='http4://data.developer.nhs.uk:8183/ccri-fhir/STU3?throwExceptionOnFailure=false&bridgeEndpoint=true' snoopy2k18/a2si-facade &
+docker run -p 8189:8188/tcp -e fhir.restserver.serverBase='http4://data.developer.nhs.uk/ccri-fhir/STU3?throwExceptionOnFailure=false&bridgeEndpoint=true' snoopy2k18/a2si-facade &

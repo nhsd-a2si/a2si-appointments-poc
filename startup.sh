a8d4129dff3f216e903fd227c964d9342b9ced44
mstephens-xsl <matthew@digital-cognition.co.uk>
2018-10-18 07:31:23 +0100

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,3 +1,16 @@
 #!/usr/bin/env bash
-docker pull snoopy2k18/a2si-facade
-docker run -p 8189:8188/tcp -e fhir.restserver.serverBase='http4://data.developer.nhs.uk/ccri-fhir/STU3?throwExceptionOnFailure=false&bridgeEndpoint=true' snoopy2k18/a2si-facade &
+sudo curl -L https://github.com/docker/compose/releases/download/1.23.0-rc2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+cd /home
+sudo mkdir poc
+cd poc
+sudo git clone https://github.com/nhsd-a2si/a2si-uec-appointment-poc
+
+cd a2si-uec-appointment-poc
+docker-compose pull
+docker-compose up -d
+sleep 20m
+
+docker cp ccri-dataload/src/main/resources/Examples/dataload.sql ccrisql:home/dataload.sql
+
+docker exec -it ccrisql psql -U fhirjpa -d careconnect -f home/dataload.sql
